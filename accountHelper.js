125ec6eb2ecc6b48fbafdff9dbf23bb08f02c734
Travis Ryan <tryan1996@gmail.com>
2018-09-27 12:18:27 -0500

    working user sessions & modular account Helper

---
+++ accountHelper.js
@@ -0,0 +1,56 @@
+/* 
+    A helper module to perform account operations in MongoDB
+*/
+
+// / Login helper function to facilitate querying the database for username/password
+module.exports.loginUser = function(username, password, mongoClient, mongoConnectionUrl, callback) {
+    mongoClient.connect(mongoConnectionUrl, function (err, db) { // Connect to MongoDB
+        if (err) { // For connection errors
+            console.log(err); // Log the error
+            callback(false); // Callback with login failure
+        } else {
+            var dbObject = db.db("pollerbear"); // Connect to the PollerBear database
+            var query = { // Object to represent the user's login attempt
+                username: username,
+                password: password
+            };
+            dbObject.collection("user").find(query).toArray(function (err, result) { // Query the user collection
+                if (err) { // For query errors
+                    console.log(err); // Log error
+                    callback(false); // Callback with login failure
+                } else {
+                    db.close(); // close Mongo connection
+                    if (result != "") // Login was successful
+                        callback(true); // callback with login success
+                    else
+                        callback(false); // Username/Password incorrect, callback with login failure
+                }
+            });
+        }
+    });
+}
+
+// Utility function for adding a new user into the database
+// TODO: Adjust logic for returning "success" or "failed" registration
+module.exports.registerUser = function(newUsername, newPassword, newEmail, mongoClient, mongoConnectionUrl, callback) {
+    mongoClient.connect(mongoConnectionUrl, function (err, db) { // Connect to MongoDB
+        if (err) throw err; // Throw connection errors
+        var dbObject = db.db("pollerbear"); // Connect to the PollerBear database
+        var newUser = { // Object to represent the new user
+            username: newUsername,
+            password: newPassword,
+            email: newEmail,
+            joinDate: new Date() // create new Date object for user's join date
+        };
+        dbObject.collection("user").insertOne(newUser, function (err, res) { // Insert function
+            if (err) { //error inserting new user
+                console.log(err); // log error
+                callback(false); // callback with failed registration
+            } else { // successful registration
+                console.log("New user has been inserted!"); // Log success message
+                db.close(); // Close connection to database
+                callback(true); // callback with successful register   
+            }
+        });
+    });
+}
--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const express = require("express"); // Express server
 const path = require("path"); // For manipulating file paths
 const fs = require("fs"); // For file I/O
 const pug = require("pug"); // Pug templating engine
+const session = require('express-session'); // Manages session variables
 const mongoClient = require('mongodb').MongoClient; // Client for connecting to MongoDB
 const bodyParser = require('body-parser'); // body parser for POST operation responses
+const accountHelper = require("./accountHelper.js"); // Helper functions in an accountHelper module
 
 // Gather secret vars (DB Username and Password)
 const dbUser = JSON.parse(fs.readFileSync("secret.json"))["dbUser"]; // database username
 const dbPassword = JSON.parse(fs.readFileSync("secret.json"))["dbPassword"]; // database password
-const mongoConnectionUrl = "mongodb://" + dbUser + ":" +  // mongo connection string
+const mongoConnectionUrl = "mongodb://" + dbUser + ":" + // mongo connection string
     encodeURIComponent(dbPassword) + "@ds115263.mlab.com:15263/pollerbear";
 
 // Setting up Express server
@@ -23,77 +25,53 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+// Configure Express to store sessions
+app.use(
+    session({
+        secret: JSON.parse(fs.readFileSync("secret.json"))["sessionSecret"], // session secret
+        key: JSON.parse(fs.readFileSync("secret.json"))["sessionKey"], // session key
+        resave: false, // don't resave session data
+        saveUninitialized: true, // save uninitialized session data
+        cookie: { // cookie settings
+            httpOnly: false, // allow other than HTTP
+            expires: new Date(Date.now() + 60 * 60 * 1000) // cookie expiration date
+        }
+    })
+);
+
 // Home Page Route
 // If user is logged in, redirect to their dashboard. Otherwise, show register/login home page
 app.get("/", function (req, res) {
-    res.render("homepage");
+    if (req.session.username) // if logged in
+        res.render("dashboard"); // render user's dashboard
+    else // if not logged in
+        res.render("homepage"); // render home page (register/login)
 });
 
 // POST method for registering a user, redirects to dashboard on success
 app.post("/register", function (req, res) {
     // Pass Request Body of parameters into a registerUser function
-    registerUser(req.body.username, req.body.password, req.body.email, function () {
-        res.render("dashboard"); // Render the user's dashboard once registered
+    accountHelper.registerUser(req.body.username, req.body.password, req.body.email, mongoClient, mongoConnectionUrl, function (registerSuccess) {
+        if(registerSuccess)
+            req.session.username = req.body.username;
+        res.redirect("/"); // Redirect to home page (will redirect to dashboard if register was successful)
     });
 });
 
-function registerUser(newUsername, newPassword, newEmail, callback) {
-    mongoClient.connect(mongoConnectionUrl, function (err, db) { // Connect to MongoDB
-        if (err) throw err; // Throw connection errors
-        var dbObject = db.db("pollerbear"); // Connect to the PollerBear database
-        var newUser = { // Object to represent the new user
-            username: newUsername,
-            password: newPassword,
-            email: newEmail,
-            joinDate: new Date() // create new Date object for user's join date
-        };
-        dbObject.collection("user").insertOne(newUser, function (err, res) { // Insert function
-            if (err) throw err; // Throw errors
-            console.log("New user has been inserted!"); // Log success message
-            db.close(); // Close connection to database
-        });
-    });
-    callback();
-}
-
-// POST method for logging in a user. On success, redirects to a user's dashboard
+// POST method for logging in a user, redirects to dashboard on success
 app.post("/login", function (req, res) {
     // Pass Request Body of parameters into a login function
-    loginUser(req.body.username, req.body.password, function (loginAttemptSuccess) {
+    accountHelper.loginUser(req.body.username, req.body.password, mongoClient, mongoConnectionUrl, function (loginAttemptSuccess) {
         if (loginAttemptSuccess) // If login was successful
-            res.render("dashboard"); // Render the user's dashboard
-        else
-            res.redirect("/"); // If login failed, redirect to the home page without logging in
+            req.session.username = req.body.username; // store session data for username
+        res.redirect("/"); // Redirect to home page (will redirect to dashboard if login was successful)
     });
 });
 
-// Login helper function to facilitate querying the database for username/password
-function loginUser(username, password, callback) {
-    mongoClient.connect(mongoConnectionUrl, function (err, db) { // Connect to MongoDB
-        if (err) { // For connection errors
-            console.log(err); // Log the error
-            callback(false); // Callback with login failure
-        } else {
-            var dbObject = db.db("pollerbear"); // Connect to the PollerBear database
-            var query = { // Object to represent the user's login attempt
-                username: username,
-                password: password
-            };
-            dbObject.collection("user").find(query).toArray(function (err, result) { // Query the user collection
-                if (err) { // For query errors
-                    console.log(err); // Log error
-                    callback(false); // Callback with login failure
-                } else {
-                    db.close(); // close Mongo connection
-                    if (result != "") // Login was successful
-                        callback(true); // callback with login success
-                    else
-                        callback(false); // Username/Password incorrect, callback with login failure
-                }
-            });
-        }
-    });
-}
+app.get("/logout", function(req, res){
+    req.session.destroy(); // destroy all session data
+    res.redirect("/"); // redirect to home page
+});
 
 app.get("/account", function (req, res) {
     res.render("dashboard");
